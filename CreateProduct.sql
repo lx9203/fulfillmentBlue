ce2bae94f8016f3a88052da3d7ba4d08164ea18e
optical3mild <chupa.pal.chad@gmail.com>
2019-05-03 17:07:10 +0900

    blue ppt update

--- CreateProduct.sql
+++
@@ -1,9 +0,0 @@
-use fulfillmentsystem;
-CREATE TABLE IF NOT EXISTS product (
-	pCode VARCHAR(10) NOT NULL,
-	pName VARCHAR(10) NOT NULL,
-	pPrice VARCHAR(10) NOT NULL,
-	pQuantity VARCHAR(10),
-	pImgSource VARCHAR(80) NOT NULL,
-	PRIMARY KEY (pCode)
-)CHARSET=utf8;
