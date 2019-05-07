f1008fa7ae269920390aa1ead9df88ce9723cdbc
lx9203 <lx9203@naver.com>
2019-05-07 12:34:23 +0900

    Feat : SupplyDAO.java, SupplyDTO.java

---
+++ src/supply/SupplyDTO.java
@@ -0,0 +1,93 @@
+package supply;
+
+import java.time.*;
+
+public class SupplyDTO {
+	private String sCode;	// 기록, 읽어오기
+	private String sProductCode;	// 기록
+	private String sProductName;	// 읽어오기
+	private int sProductPrice;	// 읽어오기
+	private String sDate;	// 기록, 읽어오기
+	private int sQuantity;	// 기록, 읽어오기
+	private int sState;	// 기록
+	private int sTotalPrice; // 기록
+	
+	public SupplyDTO(String sCode, String sProductCode, String sDate, int sQuantity, int sState) {
+		this.sCode = sCode;
+		this.sProductCode = sProductCode;
+		this.sDate = LocalDate.now()+"";
+		this.sQuantity = sQuantity;
+		this.sState = sState;
+	}
+	
+	public String getsProductName() {
+		return sProductName;
+	}
+
+	public void setsProductName(String sProductName) {
+		this.sProductName = sProductName;
+	}
+
+	public int getsProductPrice() {
+		return sProductPrice;
+	}
+
+	public void setsProductPrice(int sProductPrice) {
+		this.sProductPrice = sProductPrice;
+	}
+
+	public SupplyDTO(int sState) {
+		this.sState = sState;
+	}
+	
+	public SupplyDTO() {}
+	
+	public String getsCode() {
+		return sCode;
+	}
+
+	public void setsCode(String sCode) {
+		this.sCode = sCode;
+	}
+
+	public String getsProductCode() {
+		return sProductCode;
+	}
+
+	public void setsProductCode(String sProductCode) {
+		this.sProductCode = sProductCode;
+	}
+
+	public String getsDate() {
+		return sDate;
+	}
+
+	public void setsDate(String sDate) {
+		this.sDate = sDate;
+	}
+
+	public int getsQuantity() {
+		return sQuantity;
+	}
+
+	public void setsQuantity(int sQuantity) {
+		this.sQuantity = sQuantity;
+	}
+
+	public int getsState() {
+		return sState;
+	}
+
+	public void setsState(int sState) {
+		this.sState = sState;
+	}
+
+	public int getsTotalPrice() {
+		return sTotalPrice;
+	}
+
+	public void setsTotalPrice(int sTotalPrice) {
+		this.sTotalPrice = sTotalPrice;
+	}
+	
+}
